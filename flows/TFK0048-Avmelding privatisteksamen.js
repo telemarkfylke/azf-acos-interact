5c2d49bdffeda9feae6aebbef811d94497123e89
Robin Ellingsen <robin-elli@hotmail.com>
2024-04-12 12:25:00 +0200

    standard

--- flows/TFK0048-Avmelding privatisteksamen.js
+++ flows/TFK0048-Avmelding privatisteksamen.js
@@ -120,9 +120,9 @@ ArchiveData {
   sharepointList: {
     enabled: true,
     options: {
-     // condition: (flowStatus) => { // use this if you only need to archive some of the forms.
-     //   return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'false'
-     // },
+      // condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+      //   return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'false'
+      // },
       mapper: (flowStatus) => {
         const xmlData = flowStatus.parseXml.result.ArchiveData
         // if (!xmlData.Postnr) throw new Error('Postnr har ikke kommet med fra XML') // validation example
