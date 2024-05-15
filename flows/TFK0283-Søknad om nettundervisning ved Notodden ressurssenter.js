31f698e2bf2234a5e5402ddd24cc73fee42a074f
Robin Ellingsen <robin-elli@hotmail.com>
2024-05-15 20:16:25 +0200

    TFK0283 + TFK0009 fixes

--- flows/TFK0283-Søknad om nettundervisning ved Notodden ressurssenter.js
+++ flows/TFK0283-Søknad om nettundervisning ved Notodden ressurssenter.js
@@ -1,7 +1,7 @@
 const description = 'Sender til elevmappe'
 const { nodeEnv } = require('../config')
-const title = 'Klage - standpunkt og eksamen'
-const { schoolInfo } = require('../lib/data-sources/tfk-schools')
+const title = 'Søknad om nettundervisning ved Notodden ressurssenter'
+
 module.exports = {
   config: {
     enabled: true,
@@ -17,13 +17,7 @@ module.exports = {
   syncElevmappe: {
     enabled: true,
     options: {
-      /*
-      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
-        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
-      },
-      */
       mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
-        // Mapping av verdier fra XML-avleveringsfil fra Acos.
         return {
           ssn: flowStatus.parseXml.result.ArchiveData.Fnr
         }
@@ -77,20 +71,10 @@ module.exports = {
             Title: title,
             // UnofficialTitle: '',
             Archive: 'Sensitivt elevdokument',
-            CaseNumber: elevmappe.CaseNumber
+            CaseNumber: elevmappe.CaseNumber,
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200472' : '200249', // Team fag-, yrkes- og voksenopplæring
+            AccessGroup: 'Voksenopplæring'
           }
-        }
-
-        if (xmlData.Egendefinert1 === 'Eleveksamen eller standpunkt') {
-          const school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
-          if (!school) throw new Error(`Could not find any school with orgNr: ${xmlData.SkoleOrgNr}`)
-          documentData.parameter.ResponsibleEnterpriseNumber = xmlData.SkoleOrgNr
-          documentData.parameter.AccessGroup = school.tilgangsgruppe
-        } else if (xmlData.Egendefinert1 === 'Privatisteksamen') {
-          documentData.parameter.ResponsibleEnterpriseRecno = nodeEnv === 'production' ? '200471' : '200250' // Seksjon Sektorstøtte, inntak og eksamen
-          documentData.parameter.AccessGroup = 'Eksamen'
-        } else {
-          throw new Error('Fikk ukjent verdi inn i Egendefinert1 fra skjemaets xml-fil. Trenger "Privatisteksamen" eller "Eleveksamen')
         }
         return documentData
       }
@@ -104,33 +88,6 @@ module.exports = {
 
   closeCase: {
     enabled: false
-  },
-
-  // SP-info er kun template. Venter på info om avlevering
-  sharepointList: {
-    enabled: true,
-    options: {
-      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
-        return flowStatus.parseXml.result.ArchiveData.Egendefinert1 === 'Privatisteksamen'
-      },
-      mapper: (flowStatus) => {
-        const xmlData = flowStatus.parseXml.result.ArchiveData
-        return [
-          {
-            testListUrl: 'https://telemarkfylke.sharepoint.com/sites/T-Utdanningfolkehelseogtannhelse-Eksamen-mottakdigitaleskjemaer/Lists/Privatisteksamen%20%20Klager/AllItems.aspx',
-            prodListUrl: 'https://telemarkfylke.sharepoint.com/sites/T-Utdanningfolkehelseogtannhelse-Eksamen-mottakdigitaleskjemaer/Lists/Privatisteksamen%20%20Klager/AllItems.aspx',
-            uploadFormPdf: true,
-            uploadFormAttachments: false,
-            fields: {
-              Title: xmlData.Fnr,
-              Navn: `${xmlData.Fornavn} ${xmlData.Etternavn}`,
-              Mobilnummer: xmlData.mobilnr,
-              Fagkode: xmlData.Egendefinert2
-            }
-          }
-        ]
-      }
-    }
   },
 
   statistics: {
