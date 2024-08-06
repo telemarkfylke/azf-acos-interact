37dfcb98b8881438cb69fb8a8631d667908b3609
Robin Ellingsen <robin-elli@hotmail.com>
2024-08-06 11:52:01 +0200

    refactor: Update TFK0157-Søknadsskjema Fagskolen Vestfold og Telemark - Bransjeprogram.js

--- flows/TFK0157-Søknadsskjema Fagskolen Vestfold og Telemark - Bransjeprogram.js
+++ flows/TFK0157-Søknadsskjema Fagskolen Vestfold og Telemark - Bransjeprogram.js
@@ -1,5 +1,6 @@
-// Fagskolen er fremdeles på VTFK-løsning. Vi kjører derfor skjemaene til lokal avlevering og lar gammel løsning på VTK-task gjøre jobben
-const description = 'Søknadsskjema Fagskolen Vestfold og Telemark - Bransjeprogram'
+const description = 'Sender til Sharepoint'
+// const { nodeEnv } = require('../config')
+
 module.exports = {
   config: {
     enabled: true,
@@ -11,22 +12,65 @@ module.exports = {
     }
   },
   groundControl: {
-    enabled: true // Files will be copied to GROUND_CONTROL_STORAGE_ACCOUNT_CONTAINER_NAME, and will be downloaded on local server (./ground-control/index.js)
+    enabled: false // Files will be copied to GROUND_CONTROL_STORAGE_ACCOUNT_CONTAINER_NAME, and will be downloaded on local server (./ground-control/index.js)
   },
+
+  sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.Skjema
+        // if (!xmlData.Postnr) throw new Error('Postnr har ikke kommet med fra XML') // validation example
+        return [
+          {
+            testListUrl: 'https://telemarkfylke.sharepoint.com/sites/FAGS-Elektroniskeskjemaer/Lists/EKOMsknader',
+            prodListUrl: 'https://telemarkfylke.sharepoint.com/sites/FAGS-Elektroniskeskjemaer/Lists/EKOMsknader',
+            uploadFormPdf: true,
+            uploadFormAttachments: true,
+            fields: {
+              Title: xmlData.Fnr,
+              Fornavn: xmlData.Fornavn,
+              Etternavn: xmlData.Etternavn,
+              E_x002d_postadresse: xmlData.Epost,
+              Mobilnummer: xmlData.Mobilnr,
+              Fag_x002d_ogsvennebrev: xmlData.FagSvennebrev,
+              Annenutdanning: xmlData.AnnenUtdanning,
+              // Firmanavn: xmlData.Firmanavn,
+              Praksis_x002f_fartstid: xmlData.PraksisFartstid,
+              Adresse: xmlData.Adresse,
+              Postnummer: xmlData.PostNr,
+              Sted: xmlData.Sted,
+              // Org_x002e_nummer: xmlData.Orgnr,
+              // Fakturaadresse: xmlData.Firmaadresse,
+              Kurs: `Bransjeprogram - ${xmlData.Bransjeprogram}`
+              // nelfo: xmlData.nelfomedlem || '',
+              // oppstartmnd: xmlData.oppstartmnd || ''
+            }
+          }
+        ]
+      }
+    }
+  },
+
   statistics: {
     enabled: true,
     options: {
       mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
         // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
         return {
-          company: 'Fagskolen',
-          department: 'Bransjeprogram', // denne er antakelig feil :-)
-          description, // Required. A description of what the statistic element represents
-          type: 'søknad' // Required. A short searchable type-name that distinguishes the statistic element
+          company: 'NIK',
+          department: 'Fagskolen',
+          description,
+          type: 'Bransjeprogram' // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          // documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
         }
       }
     }
   },
+
   failOnPurpose: {
     enabled: false
   }
