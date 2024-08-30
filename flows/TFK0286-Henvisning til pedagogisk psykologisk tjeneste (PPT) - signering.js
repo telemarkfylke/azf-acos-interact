d729ac8e26936b93e70d5c06d8dd28ccb26a49d3
Tom Jarle Christiansen <fuzzbin@gmail.com>
2024-08-30 14:21:45 +0200

    Update flows for Henvisning til pedagogisk psykologisk tjeneste (PPT) + signeringsskjema

---
+++ flows/TFK0286-Henvisning til pedagogisk psykologisk tjeneste (PPT) - signering.js
@@ -0,0 +1,40 @@
+const description = 'Sender til ground control for videre behandling'
+// const { nodeEnv } = require('../config')
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseXml: {
+    enabled: true,
+    options: {
+    }
+  },
+  groundControl: {
+    enabled: true // Files will be copied to GROUND_CONTROL_STORAGE_ACCOUNT_CONTAINER_NAME, and will be downloaded on local server (./ground-control/index.js)
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'PPT',
+          department: 'Henvisning til pedagogisk-psykologisk tjeneste (PPT)',
+          description,
+          type: 'Henvisning' // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          // documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
