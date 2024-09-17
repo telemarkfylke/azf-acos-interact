013f777d033c9142e4c292361f409d56bb8347b5
Tom Jarle Christiansen <fuzzbin@gmail.com>
2024-09-17 14:24:42 +0200

    TFK0287 - Publisert

---
+++ flows/TFK0287-Tilpasset fag- og timefordeling i VGS.js
@@ -0,0 +1,117 @@
+const description = 'Sender til elevmappe'
+const { nodeEnv } = require('../config')
+const { schoolInfo } = require('../lib/data-sources/tfk-schools')
+const title = 'Tilpasset fag- og timefordeling i VGS'
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseXml: {
+    enabled: true,
+    options: {
+    }
+  },
+
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i elevmappa
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        const documentData = {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: xmlData.Fnr,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: title,
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            Status: 'J',
+            Title: title,
+            // UnofficialTitle: '',
+            Archive: 'Sensitivt elevdokument',
+            CaseNumber: elevmappe.CaseNumber,
+            // ResponsibleEnterpriseRecno: nodeEnv === 'production' ? xmlData.SkoleOrgNr : xmlData.SkoleOrgNr, // Team fag-, yrkes- og voksenopplæring
+            AccessGroup: school.tilgangsgruppe
+          }
+        }
+        return documentData
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Opplæring',
+          department: 'FAGOPPLÆRING',
+          description,
+          type: title, // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
