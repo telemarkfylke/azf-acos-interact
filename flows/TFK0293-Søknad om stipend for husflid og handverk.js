dd8237c362d211c1d24c4ff4b13d7ceabba53eb5
Tom Jarle Christiansen <fuzzbin@gmail.com>
2024-09-24 12:16:02 +0200

    Add flow for Søknad om stipend for husflid og handverk

---
+++ flows/TFK0293-Søknad om stipend for husflid og handverk.js
@@ -0,0 +1,139 @@
+const description = 'Søknad om stipend for husflid og handverk'
+const { nodeEnv } = require('../config')
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: true
+  },
+  parseXml: {
+    enabled: true
+  },
+
+  syncPrivatePerson: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i 360
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const archiveTitle = `Søknad om stipend for husflid og handverk 2025 - ${xmlData.ForNavn}`
+        const publicTitle = 'Søknad om stipend for husflid og handverk 2025'
+        const caseNumber = nodeEnv === 'production' ? '24/22469' : '24/22469'
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: xmlData.Fnr,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: archiveTitle,
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Status: 'J',
+            DocumentDate: new Date().toISOString(),
+            UnofficialTitle: archiveTitle,
+            Title: publicTitle,
+            Archive: 'Saksdokument',
+            CaseNumber: caseNumber,
+            // ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200025' : '200031', // Seksjon Kultur Dette finner du i p360, ved å trykke "Avansert Søk" > "Kontakt" > "Utvidet Søk" > så søker du etter det du trenger Eks: "Søkenavn": %Idrett%. Trykk på kontakten og se etter org nummer.
+            ResponsiblePersonEmail: 'line.ruud.orslien@telemarkfylke.no',
+            AccessCode: '5',
+            Paragraph: 'Offl. § 5',
+            AccessGroup: 'Seksjon Kultur'
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+  sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        return [
+          {
+            testListUrl: 'https://telemarkfylke.sharepoint.com/sites/SAMU-Tilskuddsordningerkulturseksjonen/Lists/Sknader%20om%20stipend%20til%20husflid%20og%20handverk/AllItems.aspx',
+            prodListUrl: 'https://telemarkfylke.sharepoint.com/sites/SAMU-Tilskuddsordningerkulturseksjonen/Lists/Sknader%20om%20stipend%20til%20husflid%20og%20handverk/AllItems.aspx',
+            uploadFormPdf: true,
+            uploadFormAttachments: true,
+            fields: {
+              Title: xmlData.ForNavn,
+              Fdato: xmlData.Fdato,
+              ForNavn: xmlData.ForNavn,
+              Etternavn: xmlData.Etternavn,
+              Telefonnummer: xmlData.Telefonnummer,
+              Epost: xmlData.Epost,
+              Hva: xmlData.Hva,
+              Maalsetting: xmlData.Maalsetting,
+              Fjoraaret: xmlData.Fjoraaret,
+              Soknadssum: xmlData.Soknadssum
+            }
+          }
+        ]
+      }
+    }
+  },
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Kultur',
+          department: '',
+          description, // Required. A description of what the statistic element represents
+          type: 'Søknad om stipend for husflid og handverk', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          documentNumber: flowStatus.archive.result.DocumentNumber // Optional. anything you like
+        }
+      }
+    }
+  },
+  failOnPurpose: {
+    enabled: false
+  }
+}
