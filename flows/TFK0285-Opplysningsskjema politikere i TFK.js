801a254cb83707f2d98ecc513b5afa05a37114d0
Tom Jarle Christiansen <fuzzbin@gmail.com>
2024-11-01 13:48:07 +0100

    Fix: Uncomment ResponsiblePersonEmail in TFK0285 flow

--- flows/TFK0285-Opplysningsskjema politikere i TFK.js
+++ flows/TFK0285-Opplysningsskjema politikere i TFK.js
@@ -70,7 +70,7 @@ module.exports = {
             Archive: 'Saksdokument',
             CaseNumber: caseNumber,
             ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200010' : '200015', // Seksjon Kultur Dette finner du i p360, ved å trykke "Avansert Søk" > "Kontakt" > "Utvidet Søk" > så søker du etter det du trenger Eks: "Søkenavn": %Idrett%. Trykk på kontakten og se etter org nummer.
-            // ResponsiblePersonEmail: 'aurora.bye.olsen@telemarkfylke.no',
+            ResponsiblePersonEmail: 'aurora.bye.olsen@telemarkfylke.no',
             AccessCode: '13',
             Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
             AccessGroup: 'Seksjon HR'
