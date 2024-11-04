bb186a02814e85d9d7e7c4a54ccb151d4357da50
Tom Jarle Christiansen <fuzzbin@gmail.com>
2024-11-04 10:18:51 +0100

    Endret doNotRemoveBlobs til false

--- flows/TFK0282-Henvisning til pedagogisk psykologisk tjeneste (PPT).js
+++ flows/TFK0282-Henvisning til pedagogisk psykologisk tjeneste (PPT).js
@@ -14,7 +14,83 @@ module.exports = {
   groundControl: {
     enabled: true // Files will be copied to GROUND_CONTROL_STORAGE_ACCOUNT_CONTAINER_NAME, and will be downloaded on local server (./ground-control/index.js)
   },
-
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr // Sjekk denne ekstra siden det er et fritekstfelt i skjemaet
+        }
+      }
+    }
+  },
+  // Arkiverer dokumentet i elevmappa
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            AccessGroup: 'Fagopplæring',
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: xmlData.fnr,
+                Role: 'Avsender',
+                IsUnofficial: false
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Henvisning til pedagogisk-psykologisk tjeneste (PPT)',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200472' : '200', // Tema fag-, yrkes- og voksenopplæring
+            // ResponsiblePersonEmail: '',
+            Status: 'J',
+            Title: 'Henvisning til pedagogisk psykologisk tjeneste (PPT)',
+            // UnofficialTitle: '',
+            Archive: 'Sensitivt elevdokument',
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+      }
+    }
+  },
   statistics: {
     enabled: true,
     options: {
