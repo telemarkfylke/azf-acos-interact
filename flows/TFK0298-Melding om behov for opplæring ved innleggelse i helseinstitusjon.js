407ea4e68dc141e362a1dfaf83805728144a92c7
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-05-12 13:45:01 +0200

    Refactor handleCase and archive options for TFK0298 flow, adding case creation logic and updating responsible enterprise record number based on environment

--- flows/TFK0298-Melding om behov for opplæring ved innleggelse i helseinstitusjon.js
+++ flows/TFK0298-Melding om behov for opplæring ved innleggelse i helseinstitusjon.js
@@ -1,3 +1,5 @@
+const { nodeEnv } = require('../config')
+
 const description = 'Skjemaer arkiveres i P360 og sendes til ground control for videre behandling'
 // const { nodeEnv } = require('../config')
 // const { schoolInfo } = require('../lib/data-sources/tfk-schools')
@@ -15,28 +17,61 @@ module.exports = {
   groundControl: {
     enabled: true // Files will be copied to GROUND_CONTROL_STORAGE_ACCOUNT_CONTAINER_NAME, and will be downloaded on local server (./ground-control/index.js)
   },
-  // Synkroniser elevmappe
-  syncElevmappe: {
+  handleCase: {
     enabled: true,
     options: {
-      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+      getCaseParameter: (flowStatus) => {
         return {
-          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+          Title: 'Opplæring i barnevern- og helseinstitusjoner', // check for exisiting case with this title
+          ArchiveCode: flowStatus.parseXml.result.ArchiveData.Fnr // Sjekker om det finnes en sak med fnr
+        }
+      },
+      mapper: (flowStatus) => {
+        return {
+          service: 'CaseService',
+          method: 'CreateCase',
+          parameter: {
+            CaseType: 'Elev',
+            Title: 'Opplæring i barnevern- og helseinstitusjoner',
+            UnofficialTitle: `Opplæring i barnevern- og helseinstitusjoner - ${flowStatus.parseXml.result.ArchiveData.Fornavn} ${flowStatus.parseXml.result.ArchiveData.Etternavn}`,
+            Status: 'B',
+            AccessCode: '13',
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            SubArchive: 'Elev',
+            ArchiveCodes: [
+              {
+                ArchiveCode: flowStatus.parseXml.result.ArchiveData.Fnr,
+                ArchiveType: 'FNR',
+                Sort: 1,
+                IsManualText: true
+              },
+              {
+                ArchiveCode: 'B31',
+                ArchiveType: 'FAGKLASSE PRINSIPP',
+                Sort: 2
+              }
+            ],
+            Contacts: [
+              {
+                Role: 'Sakspart',
+                ReferenceNumber: flowStatus.parseXml.result.ArchiveData.Fnr,
+                IsUnofficial: true
+              }
+            ],
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200027' : '200021',
+            AccessGroup: 'Opplæring helseinstitusjoner'
+          }
         }
       }
     }
   },
-
   // Arkiverer dokumentet i elevmappa
   archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
     enabled: true,
     options: {
-      // condition: (flowStatus) => { // Run archive only if isError === false.
-      //   if (flowStatus.parseXml.result.ArchiveData.Egendefinert1) { return true } else { return false } // Skal kun til arkiv hvis sjekkboks er huket av
-      // },
       mapper: (flowStatus, base64, attachments) => {
         const xmlData = flowStatus.parseXml.result.ArchiveData
-        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const caseNumber = nodeEnv === 'production' ? flowStatus.handleCase.result.CaseNumber : flowStatus.handleCase.result.CaseNumber
         const p360Attachments = attachments.map(att => {
           return {
             Base64Data: att.base64,
@@ -50,8 +85,9 @@ module.exports = {
           service: 'DocumentService',
           method: 'CreateDocument',
           parameter: {
+            CaseType: 'Elev',
             AccessCode: '13',
-            AccessGroup: 'Elev PPT', // Skal være 'Elev PPT' i prod
+            AccessGroup: 'Opplæring helseinstitusjoner',
             Category: 'Dokument inn', // 'Dokument inn' fra foresatt til PPT
             Contacts: [
               {
@@ -73,13 +109,12 @@ module.exports = {
               ...p360Attachments
             ],
             Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
-            // ResponsibleEnterpriseNumber: '200019', // Her må det være orgnr til PPT - Sjekk med arkiv
             Status: 'J',
-            Title: 'Melding om behov for opplæring ved innleggelse i helseinstitusjon',
-            UnofficialTitle: `Melding om behov for opplæring ved innleggelse i helseinstitusjon -  ${xmlData.Fornavn} ${xmlData.Etternavn}`,
+            Title: 'Opplæring i barnevern- og helseinstitusjoner',
+            UnofficialTitle: `Opplæring i barnevern- og helseinstitusjoner - ${xmlData.Fornavn} ${xmlData.Etternavn}`,
             Archive: 'Sensitivt elevdokument',
-            CaseNumber: elevmappe.CaseNumber,
-            ResponsibleEnterpriseRecno: '200027' // Seksjon PPT, OT og alternative opplæringsarenaer
+            CaseNumber: caseNumber, // Elevmappe eller Opplæring i barnevern- og helseinstitusjoner egen mappe?
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200027' : '200021' // Seksjon skoleutvikling og folkehelse
           }
         }
         return documentData
