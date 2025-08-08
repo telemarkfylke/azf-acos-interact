4b20f23dad5d76838826d590da509e0b4716ba29
mn3m <matthias.krauss@gmail.com>
2025-08-08 13:30:17 +0200

    lagt til skjema 143 og fjernet azure something tools fra package

---
+++ flows/TFK-143-Flerårig samarbeidsavtale.js
@@ -0,0 +1,151 @@
+const description = 'Sikkerhetsinstruks for informasjonssikkerhet'
+const nodeEnv = require('../config').nodeEnv
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: true
+  },
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        return {
+        }
+      }
+    }
+  },
+  syncEnterprise: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette organisasjon basert på orgnummer
+        const orgData = flowStatus.parseJson.result.DialogueInstance.Søknadsskjema.Informasjon_om_
+        return {
+          orgnr: orgData.Organisasjonsnu
+        }
+      }
+    }
+  },
+  handleCase: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          service: 'CaseService',
+          method: 'CreateCase',
+          parameter: {
+            CaseType: 'Sak',
+            Project: nodeEnv === 'production' ? '25-510' : '25-7', // Må lages nytt prosjekt for Prod i 2024,
+            Title: 'Flerårige samarbeidsavtaler på kulturfeltet',
+            UnofficialTitle: '',
+            Status: 'B',
+            AccessCode: 'U',
+            Paragraph: '',
+            JournalUnit: 'Sentralarkiv',
+            SubArchive: 'Sakarkiv',
+            ArchiveCodes: [
+              {
+                ArchiveCode: '027',
+                ArchiveType: 'FELLESKLASSE PRINSIPP',
+                Sort: 1
+              },
+              {
+                ArchiveCode: 'C00',
+                ArchiveType: 'FAGKLASSE PRINSIPP',
+                Sort: 2
+              }
+            ],
+            Contacts: [],
+            ResponsibleEnterpriseRecno: '200028', // flowStatus.syncEmployee.result.responsibleEnterprise.recno,
+            // ResponsiblePersonEmail: flowStatus.syncEmployee.result.archiveManager.email,
+            AccessGroup: '' // Automatisk
+          }
+        }
+      }
+    }
+  },
+  archive: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const caseNumber = flowStatus.handleCase.result.CaseNumber
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: 'U',
+            AccessGroup: 'Alle',
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.syncEnterprise.result.orgnr,
+                Role: 'Avsender',
+                IsUnofficial: false
+              }
+              /*,
+              {
+                ReferenceNumber: `recno: ${flowStatus.syncEmployee.result.archiveManager.recno}`,
+                Role: 'Mottaker'
+              }
+              */
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'B',
+                Title: `Skjema - flerårig samarbeidsavtale - ${flowStatus.parseJson.result.DialogueInstance.Søknadsskjema.Informasjon_om_.Navn_på_organis}`,
+                UnofficialTitle: `Skjema - flerårig samarbeidsavtale - ${flowStatus.parseJson.result.DialogueInstance.Søknadsskjema.Informasjon_om_.Navn_på_organis}`,
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: '',
+            ResponsibleEnterpriseRecno: '200028',
+            // ResponsiblePersonEmail: flowStatus.syncEmployee.result.archiveManager.email,
+            Status: 'J',
+            Title: `Flerårig samarbeidsavtale - ${flowStatus.parseJson.result.DialogueInstance.Søknadsskjema.Informasjon_om_.Navn_på_organis}`,
+            Archive: 'Saksdokument',
+            CaseNumber: caseNumber
+          }
+        }
+      }
+    }
+  },
+  signOff: {
+    enabled: false // Har med avskriving å gjøre
+  },
+
+  closeCase: { // handleCase må kjøres for å kunne kjøre closeCase
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra JSON-avleveringsfil fra dialogueportal
+        return {
+          company: 'Seksjon kultur',
+          description,
+          type: 'Flerårig samarbeidsavtale' // Required. A short searchable type-name that distinguishes the statistic element
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
