f6e9c4e296ecf128f6c2a4050bfb66b63c8b381e
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-09-02 17:34:02 +0200

    Add initial implementation for Oktobermelding flow with XML parsing and document archiving

---
+++ flows/TFK0325-oktobermelding fra kommune angående fortrinnsrett til vgo.js
@@ -0,0 +1,118 @@
+const description = '1. Oktobermelding fra kommune angående fortrinnsrett til vgo'
+const { nodeEnv } = require('../config')
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseXml: {
+    enabled: true,
+    options: {
+    }
+  },
+
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i elevmappa
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            AccessGroup: 'Elev inntak',
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.syncElevmappe.result.privatePerson.ssn,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: '1. Oktobermelding fra kommune angående fortrinnsrett til vgo',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200471' : '200250', // Seksjon Sektorstøtte, inntak og eksamen
+            // ResponsiblePersonEmail: '',
+            Status: 'J',
+            Title: '1. Oktobermelding fra kommune angående fortrinnsrett til vgo',
+            Archive: 'Sensitivt elevdokument',
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Opplæring',
+          department: 'Sektorstøtte, inntak og eksamen',
+          description,
+          type: '1. Oktobermelding fra kommune angående fortrinnsrett til vgo', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
