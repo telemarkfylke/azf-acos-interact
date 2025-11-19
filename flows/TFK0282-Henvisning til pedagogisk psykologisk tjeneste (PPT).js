9bdd6c8b2a61a51755684cbc5f4729f953642c0b
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-11-19 13:21:17 +0100

    Fix: Update school lookup logic to handle missing orgNr in TFK0282 and TFK0286 flows

--- flows/TFK0282-Henvisning til pedagogisk psykologisk tjeneste (PPT).js
+++ flows/TFK0282-Henvisning til pedagogisk psykologisk tjeneste (PPT).js
@@ -37,8 +37,8 @@ module.exports = {
       mapper: (flowStatus, base64, attachments) => {
         const xmlData = flowStatus.parseXml.result.ArchiveData
         const elevmappe = flowStatus.syncElevmappe.result.elevmappe
-        const school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
-        if (!school) throw new Error(`Could not find any school with orgNr: ${xmlData.SkoleOrgNr}`)
+        let school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
+        if (!school) { school = schoolInfo.find('929882989') }//throw new Error(`Could not find any school with orgNr: ${xmlData.SkoleOrgNr}`)
         const p360Attachments = attachments.map(att => {
           return {
             Base64Data: att.base64,
