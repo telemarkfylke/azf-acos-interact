413027f42178bdacca56e92e0d65613c814ca67b
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-11-24 13:50:37 +0100

    Fix: Update fallback school organization number in archive logic

--- flows/TFK0282-Henvisning til pedagogisk psykologisk tjeneste (PPT).js
+++ flows/TFK0282-Henvisning til pedagogisk psykologisk tjeneste (PPT).js
@@ -38,7 +38,7 @@ module.exports = {
         const xmlData = flowStatus.parseXml.result.ArchiveData
         const elevmappe = flowStatus.syncElevmappe.result.elevmappe
         let school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
-        if (!school) { school = schoolInfo.find(school => school.orgNr.toString() === '929882989') } // Fallback to default school if not found
+        if (!school) { school = schoolInfo.find(school => school.orgNr.toString() === '918124136') } // Fallback to default school if not found
         const p360Attachments = attachments.map(att => {
           return {
             Base64Data: att.base64,
@@ -62,7 +62,7 @@ module.exports = {
                 IsUnofficial: true
               },
               {
-                ReferenceNumber: '918124136', // school.orgNr, // PPT sin orgnr 918124136
+                ReferenceNumber: school.orgNr, // PPT sin orgnr 918124136
                 Role: 'Mottaker',
                 IsUnofficial: true
               }
@@ -80,7 +80,7 @@ module.exports = {
               ...p360Attachments
             ],
             Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
-            ResponsibleEnterpriseNumber: '918124136', // Denne er riktig
+            ResponsibleEnterpriseNumber: school.orgNr, // PPT sin orgnr 918124136
             Status: 'J',
             Title: 'Henvisning til pedagogisk psykologisk tjeneste',
             UnofficialTitle: `Henvisning til pedagogisk psykologisk tjeneste -  ${xmlData.Fornavn} ${xmlData.Etternavn}`,
