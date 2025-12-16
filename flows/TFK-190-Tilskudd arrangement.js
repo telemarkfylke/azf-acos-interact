f2e14dabae70c1cdf2ee2398c513c5a4802f9ea5
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-12-16 20:57:13 +0100

    Add: Implement TFK-189 and TFK-190 flows for cultural and event funding

---
+++ flows/TFK-190-Tilskudd arrangement.js
@@ -0,0 +1,145 @@
+const description = 'Tilskudd arrangement'
+const nodeEnv = require('../config').nodeEnv
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        return {
+        }
+      }
+    }
+  },
+  syncEnterprise: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette organisasjon basert på orgnummer
+        const orgData = flowStatus.parseJson.result.DialogueInstance.S\u00F8knadsskjema.Informasjon_om_.Organisasjonsnu
+        return {
+          orgnr: orgData.replaceAll(' ', '')
+        }
+      }
+    }
+  },
+
+  handleCase: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          service: 'CaseService',
+          method: 'CreateCase',
+          parameter: {
+            CaseType: 'Sak',
+            Project: nodeEnv === 'production' ? '25-881' : '25-22',
+            Title: `Tilskudd arrangement 2026 - ${flowStatus.parseJson.result.DialogueInstance.S\u00F8knadsskjema.Informasjon_om_.Navn_p\u00E5_akt\u00F8r}`,
+            UnofficialTitle: '',
+            Status: 'B',
+            AccessCode: 'U',
+            Paragraph: '',
+            JournalUnit: 'Sentralarkiv',
+            SubArchive: 'Sakarkiv',
+            ArchiveCodes: [
+              {
+                ArchiveCode: '223',
+                ArchiveType: 'FELLESKLASSE PRINSIPP',
+                Sort: 1
+              },
+              {
+                ArchiveCode: 'C03',
+                ArchiveType: 'FAGKLASSE PRINSIPP',
+                Sort: 2
+              }
+            ],
+            Contacts: [],
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200023' : '200028',
+            // ResponsiblePersonEmail: flowStatus.syncEmployee.result.archiveManager.email,
+            AccessGroup: '' // Automatisk
+          }
+        }
+      }
+    }
+  },
+  archive: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const caseNumber = flowStatus.handleCase.result.CaseNumber
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: 'U',
+            AccessGroup: 'Alle',
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.syncEnterprise.result.enterprise.EnterpriseNumber,
+                Role: 'Avsender',
+                IsUnofficial: false
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'B',
+                Title: `Tilskudd arrangement 2026 - ${flowStatus.parseJson.result.DialogueInstance.S\u00F8knadsskjema.Informasjon_om_.Navn_p\u00E5_akt\u00F8r}`,
+                UnofficialTitle: '',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: '',
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '208915' : '200028',
+            // ResponsiblePersonEmail: flowStatus.syncEmployee.result.archiveManager.email,
+            Status: 'J',
+            Title: `Søknad om tilskudd til utvikling innen kultur og reiseliv 2026 - ${flowStatus.parseJson.result.DialogueInstance.S\u00F8knadsskjema.Informasjon_om_.Navn_p\u00E5_akt\u00F8r}`,
+            Archive: 'Saksdokument',
+            CaseNumber: caseNumber
+          }
+        }
+      }
+    }
+  },
+  signOff: {
+    enabled: false // Har med avskriving å gjøre
+  },
+
+  closeCase: { // handleCase må kjøres for å kunne kjøre closeCase
+    enabled: false
+  },
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra JSON-avleveringsfil fra dialogueportal
+        return {
+          company: 'Seksjon kultur',
+          description,
+          type: 'Tilskudd arrangement' // Required. A short searchable type-name that distinguishes the statistic element
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
