536462ee308cf2ed09a7506e8a021e92ac440fb1
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2026-01-27 17:32:05 +0100

    Fix: Update flow configuration and add SharePoint list mapping for Tilskudd arrangement

--- flows/TFK-190-Tilskudd arrangement.js
+++ flows/TFK-190-Tilskudd arrangement.js
@@ -4,7 +4,7 @@ const nodeEnv = require('../config').nodeEnv
 module.exports = {
   config: {
     enabled: true,
-    doNotRemoveBlobs: false
+    doNotRemoveBlobs: true
   },
   parseJson: {
     enabled: true,
@@ -16,7 +16,7 @@ module.exports = {
     }
   },
   syncEnterprise: {
-    enabled: true,
+    enabled: false,
     options: {
       mapper: (flowStatus) => { // for å opprette organisasjon basert på orgnummer
         const orgData = flowStatus.parseJson.result.DialogueInstance.S\u00F8knadsskjema.Informasjon_om_.Organisasjonsnu
@@ -28,7 +28,7 @@ module.exports = {
   },
 
   handleCase: {
-    enabled: true,
+    enabled: false,
     options: {
       mapper: (flowStatus) => {
         return {
@@ -66,7 +66,7 @@ module.exports = {
     }
   },
   archive: {
-    enabled: true,
+    enabled: false,
     options: {
       mapper: (flowStatus, base64, attachments) => {
         const caseNumber = flowStatus.handleCase.result.CaseNumber
@@ -124,6 +124,43 @@ module.exports = {
 
   closeCase: { // handleCase må kjøres for å kunne kjøre closeCase
     enabled: false
+  },
+  sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const DialogueInstance = flowStatus.parseJson.result.DialogueInstance
+        return [
+          {
+            testListUrl: 'https://telemarkfylke.sharepoint.com/sites/SAMU-Tilskuddsordningerkulturseksjonen/Lists/TilskuddArrangement/AllItems.aspx',
+            prodListUrl: 'https://telemarkfylke.sharepoint.com/sites/SAMU-Tilskuddsordningerkulturseksjonen/Lists/TilskuddArrangement/AllItems.aspx',
+            uploadFormPdf: true,
+            uploadFormAttachments: true,
+            fields: {
+              Title: flowStatus.parseJson.result.Metadata.DialogueId.Value + ' - ' + DialogueInstance.Søknadsskjema.Informasjon_om_.Navn_på_aktør,
+              Adresse: DialogueInstance.Søknadsskjema.Informasjon_om_.Adresse,
+              // Utgift: DialogueInstance.Søknadsskjema.Utgift1[0].Utgift,
+              Beskrivelsem_x00e5_l: DialogueInstance.Søknadsskjema.Drift_og_aktivi.Beskriv_hvordan,
+              Oms_x00f8_ker: DialogueInstance.Søknadsskjema.Drift_og_aktivi.Kort_om_søker,
+              Organisasjonsnummer: DialogueInstance.Søknadsskjema.Informasjon_om_.Organisasjonsnu,
+              // inntekt: DialogueInstance.Søknadsskjema.Andre_inntekter[0].inntekt,
+              Poststed: DialogueInstance.Søknadsskjema.Informasjon_om_.Poststed,
+              Navn: DialogueInstance.Søknadsskjema.Informasjon_om_.Navn_på_aktør,
+              Sumutgift: DialogueInstance.Søknadsskjema.Sum_utgift.toString(),
+              // Navn_pa_inntekt: DialogueInstance.Søknadsskjema.Andre_inntekter[0].Navn_på_inntekt,
+              Epost: DialogueInstance.Søknadsskjema.Informasjon_om_.E_post,
+              // Navn_pa_utgift: DialogueInstance.Søknadsskjema.Utgift1[0].Navn_på_utgift,
+              // Vedlegg2: DialogueInstance.Søknadsskjema.Vedlegg2,
+              Telefonnummer: DialogueInstance.Søknadsskjema.Informasjon_om_.Telefonnummer,
+              Beskrivelseavarrangement: DialogueInstance.Søknadsskjema.Drift_og_aktivi.Beskrivelse_av_,
+              Postnummer: DialogueInstance.Søknadsskjema.Informasjon_om_.Postnummer,
+              Suminntekt: DialogueInstance.Søknadsskjema.Sum_inntekt.toString(),
+              S_x00f8_knadssum: DialogueInstance.Søknadsskjema.Økonomi1.Søknadssum.toString()
+            }
+          }
+        ]
+      }
+    }
   },
   statistics: {
     enabled: true,
