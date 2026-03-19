c0175ae2a5733239a68e3593329669a3200a1c97
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2026-03-19 08:19:25 +0100

    Add flow configuration for TFK-177 Fagskolen Klage på vedtak

---
+++ flows/TFK-177-Fagskolen Klage på vedtak.js
@@ -0,0 +1,198 @@
+const description = 'Fagskolen Klage på vedtak'
+const { nodeEnv } = require('../config')
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: nodeEnv !== 'production' // For testing purposes, we want to keep the blobs in non-prod environments
+  },
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        return {
+        }
+      }
+    }
+  },
+  // Synkroniser Student
+  syncPrivatePerson: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          ssn: flowStatus.parseJson.result.SavedValues.Login.UserID // FNR fra skjema
+        }
+      }
+    }
+  },
+
+  handleCase: {
+    enabled: true,
+    options: {
+      getCaseParameter: (flowStatus) => {
+        return {
+          Title: 'Studentmappe', // check for exisiting case with this title
+          ArchiveCode: flowStatus.parseJson.result.SavedValues.Login.UserID
+        }
+      },
+      mapper: (flowStatus) => {
+        return {
+          service: 'CaseService',
+          method: 'CreateCase',
+          parameter: {
+            CaseType: 'Elev',
+            // Project: '20-15',
+            Title: 'Studentmappe',
+            UnofficialTitle: `Studentmappe - ${flowStatus.parseJson.result.SavedValues.Login.FirstName} ${flowStatus.parseJson.result.SavedValues.Login.LastName}`,
+            Status: 'B',
+            AccessCode: '13',
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            JournalUnit: 'Fagskolen Vestfold og Telemark',
+            SubArchive: 'Student',
+            ArchiveCodes: [
+              {
+                ArchiveCode: flowStatus.parseJson.result.SavedValues.Login.UserID,
+                ArchiveType: 'FNR',
+                Sort: 1,
+                IsManualText: true
+              },
+              {
+                ArchiveCode: 'B31',
+                ArchiveType: 'FAGKLASSE PRINSIPP',
+                Sort: 2
+              }
+            ],
+            Contacts: [
+              {
+                Role: 'Sakspart',
+                ReferenceNumber: flowStatus.parseJson.result.SavedValues.Login.UserID,
+                IsUnofficial: true
+              }
+            ],
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '216024' : '200314',
+            // ResponsiblePersonEmail: flowStatus.syncPrivatePerson.result.
+            AccessGroup: 'Studentmapper' // Automatisk
+          }
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i 360
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const archiveTitle = `Klage på vedtak - ${flowStatus.parseJson.result.SavedValues.Login.FirstName} ${flowStatus.parseJson.result.SavedValues.Login.LastName}`
+        const publicTitle = 'Klage på vedtak'
+        const caseNumber = nodeEnv === 'production' ? flowStatus.handleCase.result.CaseNumber : flowStatus.handleCase.result.CaseNumber
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.parseJson.result.SavedValues.Login.UserID,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: archiveTitle,
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Status: 'J',
+            DocumentDate: new Date().toISOString(),
+            UnofficialTitle: archiveTitle,
+            Title: publicTitle,
+            Archive: 'Elevdokument',
+            CaseNumber: caseNumber,
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '216024' : '200314',
+            AccessCode: '13',
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            AccessGroup: 'Studentmapper'
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+  sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const personData = flowStatus.parseJson.result.SavedValues
+        const skjemaData = flowStatus.parseJson.result.DialogueInstance
+        return [
+          {
+            testListUrl: 'https://telemarkfylke.sharepoint.com/sites/FAGS-Studieadministrasjon/Lists/Klage%20p%20vedtak/AllItems.aspx',
+            prodListUrl: 'https://telemarkfylke.sharepoint.com/sites/FAGS-Studieadministrasjon/Lists/Klage%20p%20vedtak/AllItems.aspx',
+            uploadFormPdf: true,
+            uploadFormAttachments: true,
+            fields: {
+              Title: personData.Login.UserID.slice(0, 6),
+              fornavn: personData.Login.FirstName,
+              etternavn: personData.Login.LastName,
+              adresse: personData.Login.Address,
+              postnummer: personData.Login.PostalCode,
+              poststed: personData.Login.PostalArea,
+              mobilnummer: personData.Login.Telephone,
+              epost: personData.Login.Email,
+              klasse: skjemaData.Klagesak.Opplysninger_om.Hvilken_klasse_,
+              klagegrunn: skjemaData.Klagesak.Opplysninger_om1.Hva_gjelder_kla,
+              emne: skjemaData.Klagesak.Opplysninger_om1.Navn_p\u00E5_emne,
+              faglaerer: skjemaData.Klagesak.Opplysninger_om1.Navn_p\u00E5_fagl\u00E6re,
+              klagebeskrivelse: skjemaData.Klagesak.Begrunnelse_for.Beskriv_hva_du_,
+              bekreftelseTid: skjemaData.Klagesak.Begrunnelse_for.Jeg_bekrefter_a
+            }
+          }
+        ]
+      }
+    }
+  },
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          company: 'Fagskolen Vestfold og Telemark',
+          department: '',
+          description, // Required. A description of what the statistic element represents
+          type: 'Søknad Fagskolen', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          documentNumber: flowStatus.archive.result.DocumentNumber // Optional. anything you like
+        }
+      }
+    }
+  },
+  failOnPurpose: {
+    enabled: false
+  }
+}
