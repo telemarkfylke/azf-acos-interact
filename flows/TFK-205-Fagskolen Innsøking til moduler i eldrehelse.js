d396831249a06db26d0fad2e3609148ff86b6e20
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2026-04-13 15:41:32 +0200

    Add new flow for Fagskolen Innsøking til moduler i eldrehelse

---
+++ flows/TFK-205-Fagskolen Innsøking til moduler i eldrehelse.js
@@ -0,0 +1,199 @@
+const description = 'Fagskolen Innsøking til moduler i eldrehelse'
+const { nodeEnv } = require('../config')
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: nodeEnv !== 'production' // For testing purposes, we want to keep the blobs in non-prod environments
+  },
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        return {
+        }
+      }
+    }
+  },
+  // Synkroniser Student
+  syncPrivatePerson: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          ssn: flowStatus.parseJson.result.SavedValues.Login.UserID // FNR fra skjema
+        }
+      }
+    }
+  },
+
+  handleCase: {
+    enabled: true,
+    options: {
+      getCaseParameter: (flowStatus) => {
+        return {
+          Title: 'Studentmappe', // check for exisiting case with this title
+          ArchiveCode: flowStatus.parseJson.result.SavedValues.Login.UserID
+        }
+      },
+      mapper: (flowStatus) => {
+        return {
+          service: 'CaseService',
+          method: 'CreateCase',
+          parameter: {
+            CaseType: 'Elev',
+            Title: 'Studentmappe',
+            UnofficialTitle: `Studentmappe - ${flowStatus.parseJson.result.SavedValues.Login.FirstName} ${flowStatus.parseJson.result.SavedValues.Login.LastName}`,
+            Status: 'B',
+            AccessCode: '13',
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            JournalUnit: 'Fagskolen Vestfold og Telemark',
+            SubArchive: 'Student',
+            ArchiveCodes: [
+              {
+                ArchiveCode: flowStatus.parseJson.result.SavedValues.Login.UserID,
+                ArchiveType: 'FNR',
+                Sort: 1,
+                IsManualText: true
+              },
+              {
+                ArchiveCode: 'B31',
+                ArchiveType: 'FAGKLASSE PRINSIPP',
+                Sort: 2
+              }
+            ],
+            Contacts: [
+              {
+                Role: 'Sakspart',
+                ReferenceNumber: flowStatus.parseJson.result.SavedValues.Login.UserID,
+                IsUnofficial: true
+              }
+            ],
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '216024' : '200314',
+            // ResponsiblePersonEmail: flowStatus.syncPrivatePerson.result.
+            AccessGroup: 'Studentmapper' // Automatisk
+          }
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i 360
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const archiveTitle = `Innsøking til moduler i eldrehelse - ${flowStatus.parseJson.result.SavedValues.Login.FirstName} ${flowStatus.parseJson.result.SavedValues.Login.LastName}`
+        const publicTitle = 'Innsøking til moduler i eldrehelse'
+        const caseNumber = nodeEnv === 'production' ? flowStatus.handleCase.result.CaseNumber : flowStatus.handleCase.result.CaseNumber
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.parseJson.result.SavedValues.Login.UserID,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: archiveTitle,
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Status: 'J',
+            DocumentDate: new Date().toISOString(),
+            UnofficialTitle: archiveTitle,
+            Title: publicTitle,
+            Archive: 'Elevdokument',
+            CaseNumber: caseNumber,
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '216024' : '200314',
+            AccessCode: '13',
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            AccessGroup: 'Studentmapper'
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+  sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const personData = flowStatus.parseJson.result.SavedValues.Login
+        const skjemaData = flowStatus.parseJson.result.DialogueInstance
+        return [
+          {
+            testListUrl: 'https://telemarkfylke.sharepoint.com/sites/FAGS-Elektroniskeskjemaer/Lists/Innskning%20til%20moduler%20i%20eldrehelse/AllItems.aspx',
+            prodListUrl: 'https://telemarkfylke.sharepoint.com/sites/FAGS-Elektroniskeskjemaer/Lists/Innskning%20til%20moduler%20i%20eldrehelse/AllItems.aspx',
+            uploadFormPdf: true,
+            uploadFormAttachments: true,
+            fields: {
+              Title: flowStatus.parseJson.result.Metadata.ReferenceId.Value,
+              fnr: personData.UserID,
+              fornavn: personData.FirstName,
+              etternavn: personData.LastName,
+              adresse: personData.Address,
+              postnummer: personData.PostalCode,
+              poststed: personData.PostalArea,
+              mobilnummer: personData.Telephone,
+              epostadresse: personData.Email,
+              utdanning: skjemaData.Inns\u00F8kning_til.Opplysninger_om_soker.Utdanningsbakgrunn,
+              pri1host: skjemaData.Inns\u00F8kning_til.Modulvalg___H\u00F8s._1__prioritet,
+              pri1vinter: skjemaData.Inns\u00F8kning_til.Modulvalg___Vinter._1__prioritet2,
+              pri2vinter: skjemaData.Inns\u00F8kning_til.Modulvalg___Vinter._2__prioritet2,
+              pri1vaar: skjemaData.Inns\u00F8kning_til.Modulvalg___Var._1__prioritet3,
+              pri2vaar: skjemaData.Inns\u00F8kning_til.Modulvalg___Var._2__prioritet3,
+              gjennomfore: skjemaData.Inns\u00F8kning_til.Avkrysning.Velg
+            }
+          }
+        ]
+      }
+    }
+  },
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          company: 'Fagskolen Vestfold og Telemark',
+          department: '',
+          description, // Required. A description of what the statistic element represents
+          type: 'Søknad Fagskolen', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          documentNumber: flowStatus.archive.result.DocumentNumber // Optional. anything you like
+        }
+      }
+    }
+  },
+  failOnPurpose: {
+    enabled: false
+  }
+}
