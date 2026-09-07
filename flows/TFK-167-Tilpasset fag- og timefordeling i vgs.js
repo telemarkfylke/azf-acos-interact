8d9976dd368d4746825043010c43f0234efcb088
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2026-09-07 21:11:48 +0200

    Minor update to TFK-167 to use orgnummer from dataset instead of manual list in Acos-form

--- flows/TFK-167-Tilpasset fag- og timefordeling i vgs.js
+++ flows/TFK-167-Tilpasset fag- og timefordeling i vgs.js
@@ -32,9 +32,9 @@ module.exports = {
     options: {
       mapper: (flowStatus, base64, attachments) => {
         const jsonData = flowStatus.parseJson.result
-        const schoolName = jsonData.DialogueInstance.Om_skjemaet.Hvilken_skole_er_du_elev?.Velg_fra_rullegardinen_
-        const school = schoolInfo.find(skoleinfo => schoolName && (skoleinfo.primaryLocation === schoolName || skoleinfo.officeLocation === schoolName))
-        if (!school) throw new Error('TFK-167: Could not resolve school from selected school name')
+        const schoolOrgNr = jsonData.SavedValues?.Dataset?.Velg_fra_rullegardinen_?.OrgNr
+        const school = schoolInfo.find(skoleinfo => skoleinfo.orgNr === Number(schoolOrgNr))
+        if (!school) throw new Error('TFK-167: Could not resolve school from organisation number')
         const p360Attachments = attachments.map(att => {
           return {
             Base64Data: att.base64,
